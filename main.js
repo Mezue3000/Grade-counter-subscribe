9016056f308e1d68117418d776d6778b632d103d
mezue30000 <mezue3000>
2023-12-07 10:14:20 +0100

    Added emojis

--- main.js
+++ main.js
@@ -3,27 +3,30 @@
     inputElement.value = '';
  // Display our result on the web-page using dom
    const result = document.querySelector('.js-para')
-       
+// Function to invoke the grade button
     function checkGrade () {
         if (inputElement.value >= 0 && inputElement.value <= 40) {
             result.innerText = `your score is ${inputElement.value}, your grade is F, 
-        you fail`
+        you Failed 😥😥😥`
         } else if (inputElement.value > 40 && inputElement.value <= 49) {
             result.innerText = `your score is ${inputElement.value}, your grade is P, 
-            you Pass`
+            you Passed 😌😌😌`
         } else if (inputElement.value > 49 && inputElement.value <= 69) {
             result.innerText = `your score is ${inputElement.value}, your grade is C, 
-            Big credit to you: pls hit the subscibe button`
+            Big Credit to you: pls hit the subscibe button 😀😀😀`
         } else if (inputElement.value > 69 && inputElement.value <= 100) {
             result.innerText = `your score is ${inputElement.value}, your grade is A, 
-            Big Alpha to you: pls hit the subscribe button`
+            Big Alpha to you: pls hit the subscribe button 😍😍😍`
         } else {
             result.innerText = 'Sorry: your score is beyound the scope'
         }
 
         if (inputElement.value === '')  {
-            result.innerText =  'Hi student! input your score'
+            result.innerText =  'Hi student!👨‍👨‍👦‍👦 input your score '
         }
            inputElement.value = ''; 
     };
+
+
+
 
